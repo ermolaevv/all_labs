f8bd61ac33135b7575bec395c979a8a6fd07384f
ermolaevv <ermolaevvladislav1485@gmail.com>
2023-11-11 19:43:54 +0300

    Add Postfix lab

---
+++ expression/samples/expression_sample.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include "MyExpression.h"
+using namespace std;
+
+int main()
+{
+	try {
+		string input;
+		cout << "Enter expression: ";
+		cin >> input;
+
+		TArithmeticExpression expression(input);
+		cout << expression.GetInfix() << endl;
+		cout << expression.GetPostfix() << endl;
+		vector<string> operands = expression.GetOperands();
+
+		map<string, double> values;
+		double value;
+		for (const auto& op : operands)
+		{
+			cout << "Enter value of " << op << ": ";
+			cin >> value;
+			values[op] = value;
+		}
+		cout << expression.Calculate(values) << endl;
+	}
+	catch (exception e) {
+		cerr << e.what();
+	}
+}
